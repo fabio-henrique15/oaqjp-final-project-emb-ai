f828e4ed2879d37c55b374ebe01f7af3010d4487
fabio-henrique15 <fabiodevapps@gmail.com>
2026-01-28 23:50:14 -0500

    Refactor: Codigo server.py formatado PEP8 e nota 10 no Pylint

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+''' Executing this function initiates the application of emotion
+    detection to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,9 +10,13 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    ''' This method receives a text from the HTML and
+        runs emotion detection over it using emotion_detector()
+        function. The output returned shows shows the label and its confidence
+        score for the provided text.
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
 
@@ -20,6 +29,9 @@ def sent_analyzer():
 
 @app.route("/")
 def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
